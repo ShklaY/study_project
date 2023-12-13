54686f10fdecee93777ea480bc77e0e6c8b1ee3b
ShklaY <shekula_yana@ukr.net>
2023-12-13 17:42:09 +0200

    minor changes WEB_PAGES_LOGFILE_PATH

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 from helpers.file_system import FileSystem
 
-WEB_PAGES_LOGFILE_PATH = FileSystem.get_absolute_path_for_file(r'logs\web_pages_logfile.log')
+WEB_PAGES_LOGFILE_PATH = FileSystem.get_absolute_path_for_file('logs', 'web_pages_logfile.log')
